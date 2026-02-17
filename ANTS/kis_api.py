96aecc0c3707bfd81b18e6c239e0a63b906326b1
Kang Woo Seo <kangwoo@Kangui-MacBookAir.local>
2026-02-18 08:10:36 +0900

    fix(kis): re-apply master file download methods

--- ANTS/kis_api.py
+++ ANTS/kis_api.py
@@ -7,6 +7,9 @@ import pandas as pd
 import urllib.request
 import ssl
 import zipfile
+import urllib.request
+import ssl
+import zipfile
 
 class KisApi:
     def __init__(self, api_key, api_secret, acc_no, mock=False):
